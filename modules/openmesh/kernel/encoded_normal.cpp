8d880bf032709ec3f16c51f12041c4deeab4628c
oasin <lfod1997@gmail.com>
2026-05-14 14:43:04 +0800

    Sanitize normal-tangent update threshold basing on the encoding algorithm

--- modules/openmesh/kernel/encoded_normal.cpp
+++ modules/openmesh/kernel/encoded_normal.cpp
@@ -1,11 +1,22 @@
 ﻿#include "encoded_normal.h"
 
 #include "core/math/math_funcs.h"
+#include <limits>
+
+static _ALWAYS_INLINE_ bool is_equal_approx_within_encoding_error(const real_t p_lhs, const real_t p_rhs) {
+	static constexpr real_t ENCODE_ERROR = 2 / 65535.0;
+	static constexpr real_t ENCODE_EPSILON = ENCODE_ERROR / (4 + 3 * ENCODE_ERROR) - std::numeric_limits<real_t>::epsilon();
+	return p_lhs == p_rhs ? true : abs(p_lhs - p_rhs) <= ENCODE_EPSILON;
+}
+
+static _FORCE_INLINE_ bool is_equal_approx_within_encoding_error(const Vector3 &p_lhs, const Vector3 &p_rhs) {
+	return is_equal_approx_within_encoding_error(p_lhs.x, p_rhs.x) && is_equal_approx_within_encoding_error(p_lhs.y, p_rhs.y) && is_equal_approx_within_encoding_error(p_lhs.z, p_rhs.z);
+}
 
 void EncodedNormal::set_normal(const Vector3 &p_norm) {
 	ERR_FAIL_COND(!p_norm.is_normalized());
 	const Vector3 old_norm = get_normal();
-	if (p_norm.is_equal_approx(old_norm)) { return; }
+	if (is_equal_approx_within_encoding_error(p_norm, old_norm)) { return; }
 
 	real_t tan_d;
 	const Vector3 old_tan = get_tangent(&tan_d);
@@ -26,7 +37,7 @@ void EncodedNormal::set_tangent(const Vector3 &p_tan, real_t p_tan_dir) {
 	real_t tan_d;
 	const Vector3 old_tan = get_tangent(&tan_d);
 	const bool same_sign = signbit(tan_d) == signbit(p_tan_dir);
-	if (p_tan.is_equal_approx(old_tan)) {
+	if (is_equal_approx_within_encoding_error(p_tan, old_tan)) {
 		if (!same_sign) {
 			// Only flipping side, flip normal also.
 			set_normal_only(-get_normal());
